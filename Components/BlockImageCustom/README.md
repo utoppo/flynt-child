8aba198dafd67b57e733eefbc98d2e657027b299
utoppo <utoppo@gmail.com>
2023-06-20 13:11:00 +0200

    add copy of BlockImage for demonstration

---
+++ Components/BlockImageCustom/README.md
@@ -0,0 +1,3 @@
+# Block Image Custom
+
+Image with optional caption, multiple size options and optimized responsive image sizes. Copy of existing "Block Image", to illustrate how to add Components inside Flynt Child Theme.
---
+++ Components/BlockImageCustom/functions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Flynt\Components\BlockImageCustom;
+
+use Flynt\FieldVariables;
+
+function getACFLayout()
+{
+    return [
+        'name' => 'blockImageCustom',
+        'label' => __('Block: Image (child theme)', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => __('Image', 'flynt'),
+                'instructions' => __('Image-Format: JPG, PNG, SVG.', 'flynt'),
+                'name' => 'image',
+                'type' => 'image',
+                'preview_size' => 'medium',
+                'required' => 1,
+                'mime_types' => 'jpg,jpeg,png,svg'
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme(),
+                    [
+                        'label' => __('Size', 'flynt'),
+                        'name' => 'size',
+                        'type' => 'radio',
+                        'other_choice' => 0,
+                        'save_other_choice' => 0,
+                        'layout' => 'horizontal',
+                        'choices' => [
+                            'small' => __('Small', 'flynt'),
+                            'medium' => __('Medium', 'flynt'),
+                            'large' => __('Large (Default)', 'flynt)'),
+                            'full' => __('Full', 'flynt'),
+                        ],
+                        'default_value' => 'medium',
+                        'wrapper' =>  [
+                            'width' => '100',
+                        ],
+                    ],
+                ]
+            ]
+        ]
+    ];
+}
