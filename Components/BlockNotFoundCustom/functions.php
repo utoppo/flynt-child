06089850b01f0aa23bac2925c7e5a2c642f837b6
utoppo <utoppo@gmail.com>
2023-06-23 09:26:24 +0200

    feat(Childtheme): override parent template with locally

---
+++ Components/BlockNotFoundCustom/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flynt\Components\BlockNotFoundCustom;
+
+use Flynt\Utils\Options;
+
+Options::addTranslatable('BlockNotFoundCustom', [
+    [
+        'label' => __('General', 'flynt'),
+        'name' => 'general',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0,
+    ],
+    [
+        'label' => __('Content', 'flynt'),
+        'instructions' => __('Content to be displayed on the 404 Not Found Page', 'flynt'),
+        'name' => 'contentHtml',
+        'type' => 'wysiwyg',
+        'media_upload' => 0,
+        'default_value' => sprintf('<h1>%1$s</h1><p>%2$s</p>', __('Not Found', 'flynt'), __('The page you are looking for does not exist.', 'flynt')),
+        'required' => 1,
+        'delay' => 1,
+    ],
+    [
+        'label' => __('Back to Homepage Label', 'flynt'),
+        'instructions' => __('Leave empty to remove back to home link below the content area.', 'flynt'),
+        'name' => 'backLinkLabel',
+        'type' => 'text',
+        'default_value' => __('Back to homepage', 'flynt')
+    ]
+]);
